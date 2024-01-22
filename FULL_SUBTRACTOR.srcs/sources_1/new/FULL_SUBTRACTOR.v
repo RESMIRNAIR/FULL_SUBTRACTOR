7e83dde6f9e57d8f2762bf700f41f3d20d08c6b9
Resmi <resminair@saveetha.edu.in>
2024-01-22 14:19:32 +0530

    Updated

---
+++ FULL_SUBTRACTOR.srcs/sources_1/new/FULL_SUBTRACTOR.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.01.2024 14:44:43
+// Design Name: 
+// Module Name: FULL_SUBTRACTOR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_subtractor(a, b, c,D, Bout);
+input a, b, c;
+output D, Bout;
+                               //Write the logic for full adder in Dataflow modeling
+  
+endmodule  
